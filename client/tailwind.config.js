4867abe11a69a745cd5fe2e6fc625850b5f48d31
AirborneFluff <proffitt.m.98@gmail.com>
2024-03-16 19:45:37 +0000

    Added tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,tsx,jsx,scss,css}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
