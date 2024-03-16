baea14bcd9667948e9794285356ed54c4fba7fa7
AirborneFluff <proffitt.m.98@gmail.com>
2024-03-16 20:13:42 +0000

    Added styling options

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
     "./src/**/*.{html,ts,tsx,jsx,scss,css}",
   ],
   theme: {
+    fontFamily: {
+      sans: ['Open Sans', 'Helvetica', 'Arial', 'sans-serif'],
+      display: ['Poppins', 'Helvetica', 'Arial', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
