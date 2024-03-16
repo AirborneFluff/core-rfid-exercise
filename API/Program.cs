993a45f79407109a029cdce74c8581fb8e1b0ab9
AirborneFluff <proffitt.m.98@gmail.com>
2024-03-16 17:41:08 +0000

    Abstraction for tidy Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,23 +1,15 @@
-using API;
-using API.Data;
-using API.Data.Repositories;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(
-    o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IReportsRepository, ReportsRepository>();
+
+builder.AddDataServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
