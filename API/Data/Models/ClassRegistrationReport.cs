2c5b23f7ee8703f3488cb1150e473e90021f24e3
AirborneFluff <proffitt.m.98@gmail.com>
2024-03-16 17:47:42 +0000

    Fixed field mapping

--- API/Data/Models/ClassRegistrationReport.cs
+++ API/Data/Models/ClassRegistrationReport.cs
@@ -1,9 +1,16 @@
-﻿namespace API.Data.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Data.Models;
 
 public sealed class ClassRegistrationReport
 {
     public required string Class { get; set; }
+    
+    [Column("Teacher Name")]
     public required string TeacherName { get; set; }
+    
     public int Registrations { get; set; }
+    
+    [Column("Number Paid")]
     public int NumberPaid { get; set; }
 }
