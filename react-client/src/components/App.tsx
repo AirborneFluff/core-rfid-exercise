bb503a5bb209c87f972848f1f249e833f87a90af
AirborneFluff <proffitt.m.98@gmail.com>
2024-03-24 19:49:20 +0000

    Added tailwind and copied markup from Angular client

---
+++ react-client/src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ClassRegistrationReport from './ClassRegistrationReport';
+
+export default function App() {
+  return (
+    <div className="mt-24 max-w-screen-md mx-auto px-2">
+      <h1 className="text-4xl font-display mb-4">Reports</h1>
+      <ClassRegistrationReport />
+    </div>
+  );
+}
